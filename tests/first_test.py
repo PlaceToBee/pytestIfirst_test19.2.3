ad74fa4057391ccf92b1843bf76f0a7259e182d2
PlaceToBee <ar4i04@gmail.com>
2021-08-21 10:11:32 +0500

    19.2.3

---
+++ tests/first_test.py
@@ -0,0 +1,22 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_devision_calculator_correctly(self):
+        assert self.calc.devision(self, 4, 2) == 2
+
+    def test_subtraction_calculator_correctly(self):
+        assert self.calc.subtraction(self, 5, 1) == 4
+
+    def test_adding_calculator_correctly(self):
+        assert self.calc.adding(self, 5, 5) == 10
